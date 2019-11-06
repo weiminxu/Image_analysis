862a400aecb77ac6697b1d107a960bd7a3125264
weiminxu <weiminxu1982@gmail.com>
2019-11-06 23:30:24 +1300

    drawing line, rectangle and circle
    drawing line, rectangle and circle

--- drawing_and_writing_on_image.py
+++ drawing_and_writing_on_image.py
@@ -3,6 +3,8 @@ import cv2
 
 img = cv2.imread('D:\git\Python\Machine_Leaning\cow1.jpg', cv2.IMREAD_COLOR)
 cv2.line(img, (0,0), (150,150), (255, 255, 255), 15)
+cv2.rectangle(img, (15, 25), (200, 150), (0, 255, 0), 5)
+cv2.circle(img, (100, 63), 55, (0,0,255), -1)
 
 cv2.imshow("image", img)
 cv2.waitKey(0)
