b4648de1ac9a2b71a407b7755f44821583be1235
weiminxu <weiminxu1982@gmail.com>
2019-11-06 22:57:49 +1300

    write video into local folder
    write video into local folder

--- video_analysis.py
+++ video_analysis.py
@@ -2,11 +2,12 @@ import cv2
 import numpy as np
 
 cap = cv2.VideoCapture(0)
-
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640, 480))
 while True:
     ret, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
+    out.write(frame)
     cv2.imshow("frame", frame)
     cv2.imshow('gray', gray)
 
@@ -14,4 +15,5 @@ while True:
         break
 
 cap.release()
+out.release()
 cv2.destroyAllWindows()
