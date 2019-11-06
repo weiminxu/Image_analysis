1b5099c5fc44f2e648c4a493e54caf67424fae0f
weiminxu <weiminxu1982@gmail.com>
2019-11-06 23:45:47 +1300

    drawing polyline
    drawing polyline

--- drawing_and_writing_on_image.py
+++ drawing_and_writing_on_image.py
@@ -6,6 +6,10 @@ cv2.line(img, (0,0), (150,150), (255, 255, 255), 15)
 cv2.rectangle(img, (15, 25), (200, 150), (0, 255, 0), 5)
 cv2.circle(img, (100, 63), 55, (0,0,255), -1)
 
+pts = np.array([[10, 5],[20, 30],[70, 20],[50, 10]], np.int32)
+cv2.polylines(img, [pts], True, (0, 255, 255), 5)
+
+
 cv2.imshow("image", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
