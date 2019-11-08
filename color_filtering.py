acd05ce52fdd4cd690208e28fa772f73518bedeb
weiminxu <weiminxu1982@gmail.com>
2019-11-08 23:51:33 +1300

    burring and smoothing
    buring and smoothing

--- color_filtering.py
+++ color_filtering.py
@@ -14,10 +14,17 @@ while True:
     mask = cv2.inRange(hsv, lower_red, upper_red)
     res = cv2.bitwise_and(frame, frame, mask = mask)
 
+    kernel = np.ones((15, 15), np.float32)/225
+    smoothed = cv2.filter2D(res, -1, kernel)
+
+    blur = cv2.GaussianBlur(res, (15, 15), 0)
+
     cv2.imshow('frame', frame)
     cv2.imshow('mask', mask)
     cv2.imshow('res', res)
-
+    cv2.imshow('smoothed', smoothed)
+    cv2.imshow('blur', blur)
+    
     k = cv2.waitKey(5) & 0xFF
     if k == 27:
         break
