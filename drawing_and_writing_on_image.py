40edfcb5698edd45370929825ad54b7acfcd0621
weiminxu <weiminxu1982@gmail.com>
2019-11-10 21:56:42 +1300

    image analysis
    image analysis

--- drawing_and_writing_on_image.py
+++ drawing_and_writing_on_image.py
@@ -1,18 +1,42 @@
 import numpy as np
 import cv2
 
-img = cv2.imread('D:\git\Python\Machine_Leaning\cow1.jpg', cv2.IMREAD_COLOR)
-cv2.line(img, (0,0), (150,150), (255, 255, 255), 15)
-cv2.rectangle(img, (15, 25), (200, 150), (0, 255, 0), 5)
-cv2.circle(img, (100, 63), 55, (0,0,255), -1)
+# img = cv2.imread('D:\git\Python\Machine_Leaning\cow1.jpg', cv2.IMREAD_COLOR)
+# cv2.line(img, (0,0), (150,150), (255, 255, 255), 15)
+# cv2.rectangle(img, (15, 25), (200, 150), (0, 255, 0), 5)
+# cv2.circle(img, (100, 63), 55, (0,0,255), -1)
+#
+# pts = np.array([[10, 5],[20, 30],[70, 20],[50, 10]], np.int32)
+# cv2.polylines(img, [pts], True, (0, 255, 255), 5)
+#
+# font = cv2.FONT_HERSHEY_SIMPLEX
+# cv2.putText(img, 'OpenCV Tuts!', (0, 130), font, 1, (200, 255, 255), 2, cv2.LINE_AA)
+#
+# cv2.imshow("image", img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
 
-pts = np.array([[10, 5],[20, 30],[70, 20],[50, 10]], np.int32)
-cv2.polylines(img, [pts], True, (0, 255, 255), 5)
+#create a black image
+img = np.zeros((512, 512, 3), np.uint8)
 
+#draw a diagonal blue line with thickness of 5 px
+img = cv2.line(img, (0, 0), (511, 511), (255, 0, 0), 5)
+
+#draw a rectangle
+img = cv2.rectangle(img, (384, 0), (510, 128), (255, 0, 0), 3)
+
+#draw circle
+img = cv2.circle(img, (447, 63), 63, (0, 0, 255), -1)
+
+#draw a polygon
+pts = np.array([[10, 5],[20, 30],[70, 20],[50, 11]], np.int32)
+pts = pts.reshape((-1, 1, 2))
+img = cv2.polylines(img, [pts], True, (0, 255, 255))
+
+#add text to image
 font = cv2.FONT_HERSHEY_SIMPLEX
-cv2.putText(img, 'OpenCV Tuts!', (0, 130), font, 1, (200, 255, 255), 2, cv2.LINE_AA)
+cv2.putText(img, 'OpenCV', (10, 500), font, 4, (255, 255, 255), 2, cv2.LINE_AA)
 
-
-cv2.imshow("image", img)
+cv2.imshow('image', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
--- image_analysis.py
+++ image_analysis.py
@@ -1,22 +1,93 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
 
-img = cv2.imread('D:\git\Python\Machine_Leaning\cow1.jpg', cv2.IMREAD_GRAYSCALE)
+#img = cv2.imread('D:\git\Python\Machine_Leaning\cow1.jpg', -1)
 #IMREAD_GRAYSCALE(=0), IMREAD_COLOR(=1), IMREAD_UNCHANGED(=-1)
 
 #read image form local folder, and show it on the screen
-'''
-cv2.imshow("image", img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-'''
 
 # plt.imshow(img, cmap='gray', interpolation = 'bicubic')
 # #plt.plot([50,100],[80,100], 'c', linewith = 5)
 # plt.show()
 
-cv2.imshow("image", img)
-cv2.imwrite("cow2.jpg", img)
+# cv2.imshow("image", img)
+# cv2.imwrite("cow2.jpg", img)
 
+# cv2.imshow('cow', img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+# px = img[100, 100]
+# blue = img[100, 100, 0]
+# print(blue)
+
+#accessing red value
+# print(img.item(10, 10, 2))
+# img.itemset((10, 10, 2),100)
+# print(img.item(10, 10, 2))
+
+# print(img.shape)
+# print(img.size)
+# print(img.dtype)
+
+#splitting and merging image channels
+#b, g, r = cv2.split(img)
+#img = cv2.merge((b, g, r))
+
+#cv2.imshow('image', img)
+# b = img[:, :, 1]
+# cv2.imshow('b', b)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+#making borders for images(padding)
+# BLUE = [255, 0, 0]
+#
+# replicate = cv2.copyMakeBorder(img, 10, 10, 10, 10, cv2.BORDER_REPLICATE)
+# reflect = cv2.copyMakeBorder(img, 10, 10, 10, 10, cv2.BORDER_REFLECT)
+# reflect101 = cv2.copyMakeBorder(img, 10, 10, 10, 10, cv2.BORDER_REFLECT_101)
+# wrap = cv2.copyMakeBorder(img, 10, 10, 10, 10, cv2.BORDER_CONSTANT)
+# constant = cv2.copyMakeBorder(img, 10, 10, 10, 10, cv2.BORDER_CONSTANT, value=BLUE)
+# plt.subplot(231),plt.imshow(img, 'gray'),plt.title('ORIGINAL')
+# plt.subplot(232),plt.imshow(replicate, 'gray'),plt.title('REPLICATE')
+# plt.subplot(233),plt.imshow(reflect, 'gray'),plt.title('REFLECT')
+# plt.subplot(234),plt.imshow(reflect101,'gray'),plt.title('REFLECT_101')
+# plt.subplot(235),plt.imshow(wrap,'gray'),plt.title('WRAP')
+# plt.subplot(236),plt.imshow(constant,'gray'),plt.title('CONSTANT')
+#
+# plt.show()
+
+#add, addweighted
+# dst = cv2.addWeighted(img, 0.7, img, 0.3, 0)
+# cv2.imshow('dst', dst)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+#bitwise operations
+#load two images
+img = cv2.imread('D:\git\Python\Machine_Leaning\cow1.jpg', -1)
+
+#i want to put logo on top-left corner, so i create a ROI
+rows, cols, channels = img.shape
+roi = img[0:rows, 0:cols]
+
+#now create a mask of logo and create its inverse mask also
+img2gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+ret, mask =cv2.threshold(img2gray, 10, 255, cv2.THRESH_BINARY)
+mask_inv = cv2.bitwise_not(mask)
+
+#now black-out the area of logo in ROI
+img_bg = cv2.bitwise_and(roi, roi, mask = mask_inv)
+
+#take only region of logo from logo image
+img_fg = cv2.bitwise_and(img, img, mask = mask)
+
+#put logo in roi and modify the main image
+dst = cv2.add(img_bg, img_fg)
+img[0:rows, 0:cols] = dst
+
+cv2.imshow('res', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
